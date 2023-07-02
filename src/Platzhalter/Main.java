140432c1f660a1625e9382352509169c8adc5d10
sebastian <sebastian.schekahn@stud.htwk-leipzig.de>
2023-07-02 17:16:33 +0200

    AOP-3: change the message from Hello World to How are u, world?

--- src/Platzhalter/Main.java
+++ src/Platzhalter/Main.java
@@ -2,6 +2,6 @@ package Platzhalter;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("How are you, world?");
     }
 }
