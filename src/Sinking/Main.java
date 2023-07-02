b55435b7c6b9ccf11c9e6271b42395953223c257
anna5101 <anna.liepelt@stud.htwk-leipzig.de>
2023-07-02 15:57:48 +0200

    AOP-4: displays 1-9

--- src/Sinking/Main.java
+++ src/Sinking/Main.java
@@ -3,5 +3,6 @@ package Sinking;
 public class Main {
     public static void main(String[] args) {
         System.out.println("How are you, world?");
+        System.out.println("1 - 9");
     }
 }
