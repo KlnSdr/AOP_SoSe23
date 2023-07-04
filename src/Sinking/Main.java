0bda6ebd6cf1d162d497b6a84f3ca17c7f8d8835
KlnSdr <monsieurk1209@gmail.com>
2023-07-04 21:46:58 +0200

    AOP-31: calculate years till 21

--- src/Sinking/Main.java
+++ src/Sinking/Main.java
@@ -1,7 +1,14 @@
 package Sinking;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("How are you, world?");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("How old are you? ");
+        int age = scanner.nextInt();
+        System.out.println(21 - age + " years left until you are 21.");
     }
 }
