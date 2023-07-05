3f1905e93fb4351fb6b0fc8bd6949a2df9e2989d
KlnSdr <monsieurk1209@gmail.com>
2023-07-05 19:33:02 +0200

    AOP-32: add simple view loader class which discovers all classes in the package "Sinking.UI.views"

---
+++ src/Sinking/UI/Window.java
@@ -0,0 +1,15 @@
+package Sinking.UI;
+
+import javax.swing.*;
+
+public class Window extends JFrame {
+    public static final String baseTitle = "Schiffe versenken";
+    public Window() {
+        this.setSize(600, 400);
+        this.setLocation(100, 100);
+        this.setTitle(baseTitle);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.setVisible(true);
+    }
+}
