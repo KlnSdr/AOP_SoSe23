f2f2988dfa40f21c0ed11bccaf60d7442bb01637
KlnSdr <monsieurk1209@gmail.com>
2023-07-19 00:40:11 +0200

    AOP-9: add throws annotation to HttpHandler and IRequestHandler

--- src/Sinking/http/server/IRequestHandler.java
+++ src/Sinking/http/server/IRequestHandler.java
@@ -2,6 +2,8 @@ package Sinking.http.server;
 
 import Sinking.http.HttpHandler;
 
+import java.io.IOException;
+
 public interface IRequestHandler extends HttpHandler<IConnection> {
-    void handle(IConnection connection);
+    void handle(IConnection connection) throws IOException;
 }
