7866a5cf002c580ef2b6633b53d4438647479c29
KlnSdr <monsieurk1209@gmail.com>
2023-07-24 22:00:50 +0200

    AOP-20: remove ticket example code

--- src/Sinking/Main.java
+++ src/Sinking/Main.java
@@ -6,10 +6,6 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("How are you, world?");
-        for (int i = 0; i < 10; i++) {
-            System.out.println(i);
-        }
         SwingUtilities.invokeLater(() -> ViewLoader.getInstance().loadView("LoosingScreen"));
     }
 }
