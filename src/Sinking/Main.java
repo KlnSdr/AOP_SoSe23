1bc59d144f016de1dec2146b151cca43cc22fe8b
KlnSdr <monsieurk1209@gmail.com>
2023-07-24 22:00:09 +0200

    AOP-20: build ui in ui thread and not 'normal' thread

--- src/Sinking/Main.java
+++ src/Sinking/Main.java
@@ -2,12 +2,14 @@ package Sinking;
 
 import Sinking.UI.ViewLoader;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("How are you, world?");
         for (int i = 0; i < 10; i++) {
             System.out.println(i);
         }
-        ViewLoader.getInstance().loadView("Example1");
+        SwingUtilities.invokeLater(() -> ViewLoader.getInstance().loadView("LoosingScreen"));
     }
 }
