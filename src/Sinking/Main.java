f34737d5ea76ae753ef31916d72bc793a5297435
KlnSdr <monsieurk1209@gmail.com>
2023-07-25 19:07:18 +0200

    AOP-37: read console args into config object

--- src/Sinking/Main.java
+++ src/Sinking/Main.java
@@ -1,9 +1,12 @@
 package Sinking;
 
 import Sinking.UI.ViewLoader;
+import Sinking.http.Json;
 
 public class Main {
     public static void main(String[] args) {
         ViewLoader.getInstance().loadView("MainMenu");
+        Json config = ArgsLoader.load(args);
+        System.out.println(config);
     }
 }
