02621688555baf96f83f4bac2271070774485d01
KlnSdr <monsieurk1209@gmail.com>
2023-07-25 19:12:16 +0200

    AOP-37: evaluate console arg to device if application is in client or server mode

--- src/Sinking/Main.java
+++ src/Sinking/Main.java
@@ -5,8 +5,15 @@ import Sinking.http.Json;
 
 public class Main {
     public static void main(String[] args) {
-        ViewLoader.getInstance().loadView("MainMenu");
         Json config = ArgsLoader.load(args);
+
+        if (config.get("server").isPresent()) {
+            System.out.println("Server mode");
+        } else {
+            System.out.println("Client mode");
+            ViewLoader.getInstance().loadView("MainMenu");
+        }
+
         System.out.println(config);
     }
 }
