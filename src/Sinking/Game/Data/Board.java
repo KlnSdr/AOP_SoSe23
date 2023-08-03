a106db40f17d0999a3b445431591c93a5c556505
sebastian <sebastian.schekahn@stud.htwk-leipzig.de>
2023-08-03 17:12:21 +0200

    AOP-7 Examples for testing the board. Not really necessary. Hier sieht man dass das Schiff versteckt ist.

--- src/Sinking/Game/Data/Board.java
+++ src/Sinking/Game/Data/Board.java
@@ -18,22 +18,51 @@ public class Board {
     }
 
     public void printBoard() {
+        WaterTile w = new WaterTile();
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
-                System.out.print(board[i][j] + "\t");
+                if (board[i][j].getState() == TileState.UNKNOWN) {
+                    System.out.print(w + "\t");
+                }
+                else{
+                    System.out.print(board[i][j] + "\t");
+                }
+
             }
             System.out.print("\n");
         }
         System.out.println(board[5][5].getState());
     }
 
-    public void setShip(int x, int y) {
-        board[x][y] = new ShipTile();
+    public boolean setShip(int x, int y) {
+        boolean status = bounces(x,y);
+        if (status) {
+            board[x][y] = new ShipTile();
+        }
+        return status;
     }
 
-    public void fire(int x, int y) {
-        System.out.println("Schieß spieß");
-        board[x][y].shoot();
-        System.out.println(board[x][y].getState());
+    public boolean fire(int x, int y) {
+        boolean status = bounces(x,y);
+        if (status){
+            board[x][y].shoot();
+            System.out.println(board[x][y].getState());
+        }
+        return status;
+    }
+
+    public boolean bounces(int x, int y){
+        boolean status = false;
+
+        if (x >= 11 || x < 0) {
+            System.out.println(" Deine Eingabe (x) ist nicht mehr in den Grenzen des Spielfeldes.");
+            return status;
+        }
+        if (y >= 11 || y < 0) {
+            System.out.println(" Deine Eingabe (y) ist nicht mehr in den Grenzen des Spielfeldes.");
+            return status;
+        }
+        status = true;
+        return status;
     }
 }
--- src/Sinking/Main.java
+++ src/Sinking/Main.java
@@ -12,15 +12,16 @@ public class Main {
         int y = eingabe.next().charAt(0);
         y = Character.toUpperCase(y) - 65;
         int x = eingabe.nextInt() -1;
-       // ViewLoader.getInstance().loadView("Example1");
+        // ViewLoader.getInstance().loadView("Example1");
         Board gameboard = new Board();
         gameboard.printBoard();
-        gameboard.setShip(x, y);
+        System.out.println(gameboard.setShip(x, y));
         System.out.println("====================================================");
         gameboard.printBoard();
-        y = 1;
-        x= 1;
+        // y = 1;
+        // x= 1;
         gameboard.fire(x,y);
+        gameboard.printBoard();
 
     }
 }
