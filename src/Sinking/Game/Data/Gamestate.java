f400e70ae137f540aa0d416db2c4cfcd443561c9
sebastian <sebastian.schekahn@stud.htwk-leipzig.de>
2023-08-08 13:51:34 +0200

    AOP-8: Der Gamestate liefert das Board von Spieler 1 und zwei zurück. Des weiteren zeigt es an welcher Spielr als nächstes dran ist und gibt zurück wenn jmd gewonnen hat.

--- src/Sinking/Game/Data/Gamestate.java
+++ src/Sinking/Game/Data/Gamestate.java
@@ -8,85 +8,41 @@ public class Gamestate {
     Player spieler2 = new Player();
     
     Player winner;
+
     Board gameboardSpieler1 = new Board();
+
+
     Board gameboardSpieler2 = new Board();
     int move = 0;
 
-    public void intro(){
-        String name;
-        System.out.println("Spieler 1 gib bitte deinen Namen ein");
-        name = eingabe.next();
-        spieler1.setName(name);
-        System.out.println("Spieler 2 gib bitte deinen Namen ein");
-        name = eingabe.next();
-        spieler2.setName(name);
+
+    public Board getGameboardSpieler1() {
+        return gameboardSpieler1;
     }
 
-    public void order(){
-        int x = 0;
-        int y = 0;
-        System.out.println( spieler1 + ":" + spieler1.getHitEnemyShips());
-        System.out.println(spieler2 + ":" + spieler2.getHitEnemyShips());
+    public void setGameboardSpieler1(Board gameboardSpieler1) {
+        this.gameboardSpieler1 = gameboardSpieler1;
+    }
+
+    public Board getGameboardSpieler2() {
+        return gameboardSpieler2;
+    }
+
+    public void setGameboardSpieler2(Board gameboardSpieler2) {
+        this.gameboardSpieler2 = gameboardSpieler2;
+    }
+
+    public Player order(){
         if (move%2 == 0){
-            System.out.println(spieler1+ " schieß und Sieg");
-            y = inputY(y);
-            x = inputX(x);
-            spieler1.shoot(x,y, gameboardSpieler2);
             move++;
+            return spieler1;
         }
         else{
-            System.out.println(spieler2+ " schieß und Sieg");
-            y = inputY(y);
-            x = inputX(x);
-            spieler2.shoot(x,y, gameboardSpieler2);
             move++;
+            return spieler2;
         }
     }
 
-    //just testing
-    public void setShips(){
-        int x = 0;
-        int y = 0;
-        
-        System.out.println(spieler1+ " setze bitte dein Schiff");
-        y = inputY(y);
-        x = inputX(x);
-        gameboardSpieler1.setShip(x,y);
-        gameboardSpieler2.setShip(x,y);
-        gameboardSpieler1.setShip(x+1,y);
-        gameboardSpieler2.setShip(x+1,y);
-    }
-
-    public Player playthrough(){
-        intro();
-        setShips();
-        while (winner() == false){
-            order();
-        }
-        return winner;
-    }
-
-    public int inputY(int y){
-       do {
-           y = eingabe.next().charAt(0);
-           if(!(y >= 'A' && y <= 'Z') && !(y >= 'a' && y <= 'z')){
-               System.out.println("Falsche Eingabe");
-           }
-       }while  (!(y >= 'A' && y <= 'Z') && !(y >= 'a' && y <= 'z'));
-        y = Character.toUpperCase(y) - 65;
-        return y;
-    }
-
-    public int inputX(int x){
-        do{
-            x= eingabe.nextInt() -1;
-            if(!(x >=11 ) && !(x < 0)){
-                System.out.println("Falsche Eingabe");
-            }
-        }while(!(x < 11 && x > 0));
-
-        return x;
-    }
     
     public boolean winner(){
         
