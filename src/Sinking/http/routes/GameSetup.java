f0c7a7be516a365f25679d78364ffc1cfb74f85d
sebastian <sebastian.schekahn@stud.htwk-leipzig.de>
2023-08-10 17:14:31 +0200

    AOP-12: Dient dazu ein neues Spiel auf dem Server zu erstellen.

--- src/Sinking/http/routes/GameSetup.java
+++ src/Sinking/http/routes/GameSetup.java
@@ -1,11 +1,6 @@
 package Sinking.http.routes;
 
-import Sinking.Game.Data.Server.Exceptions.GameFinishedException;
-import Sinking.Game.Data.Server.Exceptions.GameNotFoundException;
-import Sinking.Game.Data.Server.Exceptions.NoPlayerNeededException;
 import Sinking.Game.Data.Server.GameRepository;
-import Sinking.Game.Data.Server.ServerGamestate;
-import Sinking.Game.Data.Server.ServerPlayerWrapper;
 import Sinking.http.Json;
 import Sinking.http.server.Annotations.Get;
 import Sinking.http.server.IConnection;
@@ -16,7 +11,7 @@ import java.util.UUID;
 public class GameSetup {
 
     @Get(route = "/Schiffeversenken/Titanic/Ahoi")
-    public void SpielErstellen(IConnection verbindung) throws IOException, GameFinishedException, NoPlayerNeededException, GameNotFoundException {
+    public void SpielErstellen(IConnection verbindung){
         Json msg = new Json();
         msg.set("Spiel", "Schiffeversenken");
 
